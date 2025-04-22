a0e898dfcbc79442a69bbea69f3dea12d17be07f
misosucky <126988962+Misosucky@users.noreply.github.com>
2025-04-22 14:34:22 -0400

    text from book, pg2

--- learn-python-the-hard-way/exercises/ex3.py
+++ learn-python-the-hard-way/exercises/ex3.py
@@ -16,3 +16,9 @@ print("What is 3 + 2?", 3 + 2)
 print ("What is 5 - 7?", 5 - 7)
 
 print ("oh, that's why it's False.")
+
+print ("How about some more.")
+
+print("Is it greater?", 5 > -2)
+print("Is it greater or equal?", 5 >= -2)
+print("Is it less or equal?", 5 <= -2)
