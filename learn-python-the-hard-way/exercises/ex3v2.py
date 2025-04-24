70e01ea8a0158051c49e5fd9cb47161a39bc5ba1
misosucky <126988962+Misosucky@users.noreply.github.com>
2025-04-24 14:58:07 -0400

    ex3.v2 bill calculator

---
+++ learn-python-the-hard-way/exercises/ex3v2.py
@@ -0,0 +1,13 @@
+#Monthly Expense Example
+Rent(-700)
+Electricity(-125)
+Internet(-75)
+Phone(-180)
+Gas(-120)
+Groceries(-800)
+Car(-350)
+Income(3500)
+
+monProf (Rent+Electricity+Internet+Phone+Gas+Groceries+Car+Income)
+
+Print (monPro)
