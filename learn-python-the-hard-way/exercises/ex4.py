ebecd3c2304d2f5105db5f93a1970e4714729c13
misosucky <126988962+Misosucky@users.noreply.github.com>
2025-04-25 14:33:57 -0400

    typos

--- learn-python-the-hard-way/exercises/ex4.py
+++ learn-python-the-hard-way/exercises/ex4.py
@@ -8,7 +8,7 @@ passengers = 90
 cars_not_driven = cars - drivers
 cars_driven = drivers
 carpool_capacity = cars_driven * space_in_a_car
-average_passangers_per_car = passnagers/cars_driven
+average_passengers_per_car = passengers/cars_driven
 
 #uses the vars and interactions (also vars) to print
 #use , to add car to string
@@ -17,4 +17,4 @@ print("There are only", drivers, "drivers available.")
 print("There will be", cars_not_driven, "empty cars today.")
 print("We can trasnport", carpool_capacity, "people today.")
 print("We have", passengers, "to carpool today.")
-print("We need to put about", average_passangers_per_car, "in each car")
+print("We need to put about", average_passengers_per_car, "in each car")
