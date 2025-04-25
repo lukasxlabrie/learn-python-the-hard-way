04166952d3efcf657e49b225b34bbe728b273c8a
misosucky <126988962+Misosucky@users.noreply.github.com>
2025-04-25 14:31:40 -0400

    code notes

--- learn-python-the-hard-way/exercises/ex4.py
+++ learn-python-the-hard-way/exercises/ex4.py
@@ -1,11 +1,16 @@
+#vars defined as numbers
 cars = 100
 space_in_a_car = 4.0
 drivers = 30
+
+#takes the above vars and makes them interact
 cars_not_driven = cars - drivers
 cars_driven = drivers
 carpool_capacity = cars_driven * space_in_a_car
 average_passangers_per_car - passnagers/cars_driven
 
+#uses the vars and interactions (also vars) to print
+#use , to add car to string
 print("There are", cars, "cars available.")
 print("There are only", drivers, "drivers available.")
 print("There will be", cars_not_driven, "empty cars today.")
