1f6a72a740774fcc880185aa219cf0d560b1fb5a
misosucky <126988962+Misosucky@users.noreply.github.com>
2025-04-29 13:31:24 -0400

    added notes

--- learn-python-the-hard-way/exercises/ex7.py
+++ learn-python-the-hard-way/exercises/ex7.py
@@ -1,8 +1,9 @@
-print("Mary had a little lamb.")
-print("Its fleece was white as {}.".format('snow'))
-print ("and everywhere that MAry went.")
-print("." * 10)
+print("Mary had a little lamb.") #prints a string
+print("Its fleece was white as {}.".format('snow')) # prints a string and uses .format to insert snow, {} is placeholder and uses () to fill the place
+print ("and everywhere that Mary went.") # prints a string
+print("." * 10) # prints 10 .
 
+# a list of vars which are strings
 end1 = "C"
 end2 = "h"
 end3 = "e"
@@ -16,6 +17,6 @@ end10 = "g"
 end11 = "e"
 end12 = "r"
 
-# watch the coma at the end, try removing it to see what happens
-print(end1 + end2 + end3 + end4 + end5 + end6, end= '')
+# using the end = '' tells the program to avoid starting a new line, can be used to add a custom field or add space as well, see notes doc
+print(end1 + end2 + end3 + end4 + end5 + end6, end = '' )
 print(end7 + end8 + end9 + end10 + end11 + end12)
