de59527c5c1a883c2a86415ec057dd32bfda35d4
misosucky <126988962+Misosucky@users.noreply.github.com>
2025-04-29 13:45:06 -0400

    added notes

--- learn-python-the-hard-way/exercises/ex8.py
+++ learn-python-the-hard-way/exercises/ex8.py
@@ -1,13 +1,17 @@
+# string with 4 placeholders
 formatter = "{} {} {} {}"
 
+# each line tells the prgam to print formatter but fill in the placeholders with whats between the ()
+# each .format must have 4 vars to fill in the placeholders
 print(formatter.format(1, 2, 3, 4))
 print(formatter.format("one", "two", "three", "four"))
 print(formatter.format(True, False, False, True))
 print(formatter.format(formatter, formatter, formatter, formatter))
 
+# this approch makes it easier to read whats happening
 print (formatter.format(
-    "Try your",
-    "Own text here",
-    "Maybe a poem",
-    "Or a song about fear"
+    "Writting code like this",
+    "Makes the program easier to read",
+    "I think its common practice for loops and in Javascript",
+    "I am not sure if it's nomral for Python coding"
 ))
