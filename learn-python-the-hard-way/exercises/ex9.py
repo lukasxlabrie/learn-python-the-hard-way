1bf468127b3efcd98c4bfc8a0de0c7439dc9086c
misosucky <126988962+Misosucky@users.noreply.github.com>
2025-04-29 13:51:41 -0400

    typo

--- learn-python-the-hard-way/exercises/ex9.py
+++ learn-python-the-hard-way/exercises/ex9.py
@@ -1,7 +1,7 @@
 # Heres some new strange stuff, remember type it exactly
 
 days = "Mon Tue Wed Thu Fri Sat Sun"
-months = "Jan\nFeb\nMar\nApr\nJun\nJul\nAug\nSep\nOct\nNov\nDec"
+months = "\nJan\nFeb\nMar\nApr\nJun\nJul\nAug\nSep\nOct\nNov\nDec"
 
 print ("Here are the days: ", days)
 print("Here are the months:", months)
