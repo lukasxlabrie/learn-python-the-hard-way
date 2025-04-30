906dc3495e1de1e556dc07c1ffde59f4067469d2
misosucky <126988962+Misosucky@users.noreply.github.com>
2025-04-30 13:20:44 -0400

    notes

--- learn-python-the-hard-way/exercises/ex11.py
+++ learn-python-the-hard-way/exercises/ex11.py
@@ -1,12 +1,11 @@
 # prints the string and stays on the same line (does not add a newline)
-print ("How old are you?")
+print ("How old are you?", end=' ')
 
 # prompts the user for input and stores it in a variable
 age = input()
-
-print ("How tall are you?")
+print ("How tall are you?", end=' ')
 height = input()
-print ("How much do you weigh?")
+print ("How much do you weigh?", end=' ')
 weight = input()
 
 # f-string that inserts the user’s input (stored in variables) into the sentence
