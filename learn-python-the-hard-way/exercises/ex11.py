e447736da9e60ff40085adbd468628e4035fb26e
misosucky <126988962+Misosucky@users.noreply.github.com>
2025-04-30 13:08:01 -0400

    book text

---
+++ learn-python-the-hard-way/exercises/ex11.py
@@ -0,0 +1,8 @@
+print ("How old are you?", end=' ')
+age = input()
+print ("How tall are you?", end=' ')
+height = inout()
+print ("How much do you weigh?", end=' ')
+weight = input()
+
+print (f"So, you're{age} oldm {heigh} tall and {weight} heavy.")
