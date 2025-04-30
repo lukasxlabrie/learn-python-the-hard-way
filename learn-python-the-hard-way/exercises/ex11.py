969812574bd97fa7472c3ec54b3f324bfde465a6
misosucky <126988962+Misosucky@users.noreply.github.com>
2025-04-30 13:09:45 -0400

    typo/grammar

--- learn-python-the-hard-way/exercises/ex11.py
+++ learn-python-the-hard-way/exercises/ex11.py
@@ -1,8 +1,8 @@
 print ("How old are you?", end=' ')
 age = input()
 print ("How tall are you?", end=' ')
-height = inout()
+height = input()
 print ("How much do you weigh?", end=' ')
 weight = input()
 
-print (f"So, you're{age} oldm {heigh} tall and {weight} heavy.")
+print (f"So, you're {age} old, {height} tall, and {weight} heavy.")
