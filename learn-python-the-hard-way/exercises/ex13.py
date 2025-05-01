488d76008f7ca48d9730facabe143719e8cb0a2e
misosucky <126988962+Misosucky@users.noreply.github.com>
2025-05-01 15:42:31 -0400

    notes

--- learn-python-the-hard-way/exercises/ex13.py
+++ learn-python-the-hard-way/exercises/ex13.py
@@ -1,8 +1,10 @@
+# this tells the program that we will be using argv, which is on my sysyem later in the program
 from sys import argv
-# read the WYSS section for how to run this
 
+# argv has 4 variables, assigned left to rightß
 script, first, second, third = argv
 
+# prints that use whats stored in argv 
 print ("This script is called:", script)
 print ("Your first variable is:", first)
 print ("Your second variable is:", second)
