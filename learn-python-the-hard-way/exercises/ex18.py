06f819eb8e9e116e2863dab20a8b8be899318538
misosucky <126988962+Misosucky@users.noreply.github.com>
2025-06-03 14:36:57 -0400

    added notes to note.md

--- learn-python-the-hard-way/exercises/ex18.py
+++ learn-python-the-hard-way/exercises/ex18.py
@@ -1,4 +1,5 @@
-# this one is like your scripts with argv
+# this one is like your scripts with argv, def is a funciton
+# function = mini script 
 def print_two(*args):
     arg1,arg2 = args
     print(f"arg1: {arg1}, arg2: {arg2}")
@@ -15,6 +16,8 @@ def print_one(arg1):
 def print_none():
     print("I got nothin'.")
 
+# prints all version sof function
+# calls/runs/sues function
 print_two("Zed", "Shaw")
 print_two_again("Zed", "Shaw")
 print_one("First")
