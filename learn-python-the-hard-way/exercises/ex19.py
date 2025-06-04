47bb1e6dbc60fce5b92db062f53fb3954dde3a87
misosucky <126988962+Misosucky@users.noreply.github.com>
2025-06-04 14:07:42 -0400

    function call and vars

--- learn-python-the-hard-way/exercises/ex19.py
+++ learn-python-the-hard-way/exercises/ex19.py
@@ -4,4 +4,10 @@ def chesse_and_crackers(cheese_count, boxes_of_crackers):
     print("Man that's enough for a party!")
     print("Get a blanket.\n")
 
-    
+    print ("We can just give the function numbers directly:")
+    cheese_and_crackers(20,30)
+
+    print("OR, we can use variables from our script:")
+    amount_of_cheese = 10
+    amount_of_crackers = 50
+
