1e793b0c29db91e397b497f645759920a03f53bd
misosucky <126988962+Misosucky@users.noreply.github.com>
2025-06-04 14:09:09 -0400

    dedented

--- learn-python-the-hard-way/exercises/ex19.py
+++ learn-python-the-hard-way/exercises/ex19.py
@@ -4,11 +4,13 @@ def cheese_and_crackers(cheese_count, boxes_of_crackers):
     print("Man that's enough for a party!")
     print("Get a blanket.\n")
 
-    print ("We can just give the function numbers directly:")
-    cheese_and_crackers(20,30)
+print ("We can just give the function numbers directly:")
+cheese_and_crackers(20,30)
 
-    print("OR, we can use variables from our script:")
-    amount_of_cheese = 10
-    amount_of_crackers = 50
+print("OR, we can use variables from our script:")
+amount_of_cheese = 10
+amount_of_crackers = 50
 
 cheese_and_crackers(amount_of_cheese, amount_of_crackers)
+
+print
