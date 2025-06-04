df38b55675bb7b95cbb0c8c2cbd2bd4fadb2a46a
misosucky <126988962+Misosucky@users.noreply.github.com>
2025-06-04 14:13:46 -0400

    added inline notes

--- learn-python-the-hard-way/exercises/ex19.py
+++ learn-python-the-hard-way/exercises/ex19.py
@@ -1,20 +1,24 @@
+# defines a function that takes 2 arguments
 def cheese_and_crackers(cheese_count, boxes_of_crackers):
-    print(f"You have{cheese_count} cheeses!)")
-    print(f"You have {boexes_of_crackers} boxes of crackers!")
+    print(f"You have {cheese_count} cheeses!")
+    print(f"You have {boxes_of_crackers} boxes of crackers!")
     print("Man that's enough for a party!")
     print("Get a blanket.\n")
 
-print ("We can just give the function numbers directly:")
-cheese_and_crackers(20,30)
+# call the function using numbers
+print("We can just give the function numbers directly:")
+cheese_and_crackers(20, 30)
 
+# call the function using variables
 print("OR, we can use variables from our script:")
 amount_of_cheese = 10
 amount_of_crackers = 50
-
 cheese_and_crackers(amount_of_cheese, amount_of_crackers)
 
+# call the function using math
 print("We can even do math inside too:")
-cheese_and_crackers(10+20, 5+6)
+cheese_and_crackers(10 + 20, 5 + 6)
 
+# call the function using variables and math
 print("And we can combine the two, variables and math:")
 cheese_and_crackers(amount_of_cheese + 100, amount_of_crackers + 1000)
