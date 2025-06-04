190be5de4af8ecfad8fbbdf6b994f470d1c37b26
misosucky <126988962+Misosucky@users.noreply.github.com>
2025-06-04 14:08:27 -0400

    typo

--- learn-python-the-hard-way/exercises/ex19.py
+++ learn-python-the-hard-way/exercises/ex19.py
@@ -1,4 +1,4 @@
-def chesse_and_crackers(cheese_count, boxes_of_crackers):
+def cheese_and_crackers(cheese_count, boxes_of_crackers):
     print(f"You have{cheese_count} cheeses!)")
     print(f"You have {boexes_of_crackers} boxes of crackers!")
     print("Man that's enough for a party!")
@@ -11,3 +11,4 @@ def chesse_and_crackers(cheese_count, boxes_of_crackers):
     amount_of_cheese = 10
     amount_of_crackers = 50
 
+cheese_and_crackers(amount_of_cheese, amount_of_crackers)
